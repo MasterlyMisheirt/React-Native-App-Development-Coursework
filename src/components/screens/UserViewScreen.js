a4bc60364d1685c662877309bbfc5d6c620fb077
MasterlyMisheirt <thewizardofmich@gmail.com>
2024-11-22 11:28:45 +0000

    Added UserViewScreen

---
+++ src/components/screens/UserViewScreen.js
@@ -0,0 +1,19 @@
+import Screen from "../layout/Screen";
+import UserView from "../Entity/Users/UserView";
+
+export const UserViewScreen = ({ navigation, route }) => {
+  //Initialisations -------------
+  const { user, onDelete, onModify } = route.params;
+  //State -----------------------
+  //Handlers --------------------
+  const goToModifyScreen = () =>
+    navigation.navigate("UserModifyScreen", { user, onModify });
+  //View ------------------------
+  return (
+    <Screen>
+      <UserView user={user} onDelete={onDelete} onModify={goToModifyScreen} />
+    </Screen>
+  );
+};
+
+export default UserViewScreen;
