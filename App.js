693a43461e6766ef42ac87a82e9e197060e3797e
MasterlyMisheirt <thewizardofmich@gmail.com>
2024-11-22 18:27:18 +0000

    Squashed commit of the following:
    commit 605548e92c9950bd36492fe02889fd7991cb1f3c
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 17:05:55 2024 +0000
    
        Implemented a drawer
    
        The drawer contains user crudler and module crudler, which can be navigated
    
    commit ce255d7fa7841679a836e78b51bd96ccdf6ee8db
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 15:09:14 2024 +0000
    
        Imported drawer dependency
    
    commit 11ed919cdda7baf29b916581a30b57e52ab21ffc
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 12:21:21 2024 +0000
    
        Added UserModifyScreen
    
        ADDITIONAL:
        Removed redundant code in ModuleModifyScreen
    
    commit 31d900343e1891f40115ea6f0bbd73842236339f
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 12:18:26 2024 +0000
    
        Changed lecturer information for users.js
    
    commit 5246f630385ba6924cff16a9056fd4579a6ca416
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 12:18:08 2024 +0000
    
        Added UserView
    
    commit a4bc60364d1685c662877309bbfc5d6c620fb077
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 11:28:45 2024 +0000
    
        Added UserViewScreen
    
    commit 3441a0630627c61629396a20d5ef07178d5b0c32
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Fri Nov 22 11:21:28 2024 +0000
    
        Bug fix and code cleaning #1
    
        -Removed redundant code in UserListScreen
        -Updated users.js to export default
    
    commit 3f1077a43b9bec79c6e4856f16b30543d55e3bdc
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Thu Nov 21 19:42:57 2024 +0000
    
        Added UserItem
    
    commit c2f6c1c30144445a13765631675eef19a194d134
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Thu Nov 21 19:33:34 2024 +0000
    
        Added UserList
    
    commit a7362a058d48dde48051f90cea5df0bde0430d73
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Thu Nov 21 19:22:28 2024 +0000
    
        Added UserListScreen
    
    commit 6132319800ac3445d1ede8951df2c2937c575129
    Author: MasterlyMisheirt <thewizardofmich@gmail.com>
    Date:   Thu Nov 21 19:00:34 2024 +0000
    
        Added UserForm

--- App.js
+++ App.js
@@ -1,67 +1,94 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import UserListScreen from "./src/components/screens/UserListScreen";
+import UserAddScreen from "./src/components/screens/UserAddScreen";
+import UserModifyScreen from "./src/components/screens/UserModifyScreen";
+import UserViewScreen from "./src/components/screens/UserViewScreen";
 import ModuleListScreen from "./src/components/screens/ModuleListScreen";
 import ModuleAddScreen from "./src/components/screens/ModuleAddScreen";
 import ModuleModifyScreen from "./src/components/screens/ModuleModifyScreen";
 import ModuleViewScreen from "./src/components/screens/ModuleViewScreen";
-import UserListScreen from "./src/components/screens/UserListScreen";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-export const App = () => {
-  //Initialisations -------------
-  //State -----------------------
-  //Handlers --------------------
-  //View ------------------------
+const UserNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="UserListScreen"
-        screenOptions={{
-          headerStyle: { backgroundColor: "black" },
-          headerTintColor: "white",
-          statusBarColor: "black",
-        }}
-      >
-        <Stack.Screen
-          name="UserListScreen"
-          component={UserListScreen}
-          options={{ title: "List users" }}
-        />
-
-        <Stack.Screen
-          name="ModuleListScreen"
-          component={ModuleListScreen}
-          options={{ title: "List modules" }}
-        />
-        <Stack.Screen
-          name="ModuleAddScreen"
-          component={ModuleAddScreen}
-          options={{ title: "Add modules" }}
-        />
-        <Stack.Screen
-          name="ModuleModifyScreen"
-          component={ModuleModifyScreen}
-          options={{ title: "Modify modules" }}
-        />
-        <Stack.Screen
-          name="ModuleViewScreen"
-          component={ModuleViewScreen}
-          options={{ title: "View modules" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="UserListScreen"
+      screenOptions={{
+        headerStyle: { backgroundColor: "black" },
+        headerTintColor: "white",
+        statusBarColor: "black",
+      }}
+    >
+      <Stack.Screen
+        name="UserListScreen"
+        component={UserListScreen}
+        options={{ title: "List users" }}
+      />
+      <Stack.Screen
+        name="UserAddScreen"
+        component={UserAddScreen}
+        options={{ title: "Add users" }}
+      />
+      <Stack.Screen
+        name="UserModifyScreen"
+        component={UserModifyScreen}
+        options={{ title: "Modify users" }}
+      />
+      <Stack.Screen
+        name="UserViewScreen"
+        component={UserViewScreen}
+        options={{ title: "View users" }}
+      />
+    </Stack.Navigator>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const ModuleNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="ModuleListScreen"
+      screenOptions={{
+        headerStyle: { backgroundColor: "black" },
+        headerTintColor: "white",
+        statusBarColor: "black",
+      }}
+    >
+      <Stack.Screen
+        name="ModuleListScreen"
+        component={ModuleListScreen}
+        options={{ title: "List modules" }}
+      />
+      <Stack.Screen
+        name="ModuleAddScreen"
+        component={ModuleAddScreen}
+        options={{ title: "Add modules" }}
+      />
+      <Stack.Screen
+        name="ModuleModifyScreen"
+        component={ModuleModifyScreen}
+        options={{ title: "Modify modules" }}
+      />
+      <Stack.Screen
+        name="ModuleViewScreen"
+        component={ModuleViewScreen}
+        options={{ title: "View modules" }}
+      />
+    </Stack.Navigator>
+  );
+};
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="User Crudler" component={UserNavigator} />
+        <Drawer.Screen name="Module Crudler" component={ModuleNavigator} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
