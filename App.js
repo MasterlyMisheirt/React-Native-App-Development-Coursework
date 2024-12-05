4b7444f23c01ebc149cc1068367cae1d8f54d322
MasterlyMisheirt <thewizardofmich@gmail.com>
2024-12-05 08:32:17 +0000

    Sorted out App.js code to make it neater

--- App.js
+++ App.js
@@ -12,84 +12,82 @@ import ModuleModifyScreen from "./src/components/screens/ModuleModifyScreen";
 import ModuleViewScreen from "./src/components/screens/ModuleViewScreen";
 
 const Stack = createStackNavigator();
+
+const ModuleNavigation = () => (
+  <Stack.Navigator
+    initialRouteName="ModuleListScreen"
+    screenOptions={{
+      headerStyle: { backgroundColor: "black" },
+      headerTintColor: "white",
+      statusBarColor: "black",
+    }}
+  >
+    <Stack.Screen
+      name="ModuleListScreen"
+      component={ModuleListScreen}
+      options={{ title: "List modules" }}
+    />
+    <Stack.Screen
+      name="ModuleAddScreen"
+      component={ModuleAddScreen}
+      options={{ title: "Add modules" }}
+    />
+    <Stack.Screen
+      name="ModuleModifyScreen"
+      component={ModuleModifyScreen}
+      options={{ title: "Modify modules" }}
+    />
+    <Stack.Screen
+      name="ModuleViewScreen"
+      component={ModuleViewScreen}
+      options={{ title: "View modules" }}
+    />
+  </Stack.Navigator>
+);
+
+const UserNavigation = () => (
+  <Stack.Navigator
+    initialRouteName="UserListScreen"
+    screenOptions={{
+      headerStyle: { backgroundColor: "black" },
+      headerTintColor: "white",
+      statusBarColor: "black",
+    }}
+  >
+    <Stack.Screen
+      name="UserListScreen"
+      component={UserListScreen}
+      options={{ title: "List users" }}
+    />
+    <Stack.Screen
+      name="UserAddScreen"
+      component={UserAddScreen}
+      options={{ title: "Add users" }}
+    />
+    <Stack.Screen
+      name="UserModifyScreen"
+      component={UserModifyScreen}
+      options={{ title: "Modify users" }}
+    />
+    <Stack.Screen
+      name="UserViewScreen"
+      component={UserViewScreen}
+      options={{ title: "View users" }}
+    />
+  </Stack.Navigator>
+);
+
 const Drawer = createDrawerNavigator();
 
-const UserNavigator = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="UserListScreen"
-      screenOptions={{
-        headerStyle: { backgroundColor: "black" },
-        headerTintColor: "white",
-        statusBarColor: "black",
-      }}
-    >
-      <Stack.Screen
-        name="UserListScreen"
-        component={UserListScreen}
-        options={{ title: "List users" }}
-      />
-      <Stack.Screen
-        name="UserAddScreen"
-        component={UserAddScreen}
-        options={{ title: "Add users" }}
-      />
-      <Stack.Screen
-        name="UserModifyScreen"
-        component={UserModifyScreen}
-        options={{ title: "Modify users" }}
-      />
-      <Stack.Screen
-        name="UserViewScreen"
-        component={UserViewScreen}
-        options={{ title: "View users" }}
-      />
-    </Stack.Navigator>
-  );
-};
+const DrawerScreen = () => (
+  <Drawer.Navigator initialRouteName="ModuleListScreen">
+    <Drawer.Screen name="Module Crudler" component={ModuleNavigation} />
+    <Drawer.Screen name="User Crudler" component={UserNavigation} />
+  </Drawer.Navigator>
+);
 
-const ModuleNavigator = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="ModuleListScreen"
-      screenOptions={{
-        headerStyle: { backgroundColor: "black" },
-        headerTintColor: "white",
-        statusBarColor: "black",
-      }}
-    >
-      <Stack.Screen
-        name="ModuleListScreen"
-        component={ModuleListScreen}
-        options={{ title: "List modules" }}
-      />
-      <Stack.Screen
-        name="ModuleAddScreen"
-        component={ModuleAddScreen}
-        options={{ title: "Add modules" }}
-      />
-      <Stack.Screen
-        name="ModuleModifyScreen"
-        component={ModuleModifyScreen}
-        options={{ title: "Modify modules" }}
-      />
-      <Stack.Screen
-        name="ModuleViewScreen"
-        component={ModuleViewScreen}
-        options={{ title: "View modules" }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      UserNavigator();
-      <Drawer.Navigator>
-        <Drawer.Screen name="User Crudler" component={UserNavigator} />
-        <Drawer.Screen name="Module Crudler" component={ModuleNavigator} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
+export default () => (
+  <NavigationContainer>
+    <DrawerScreen />
+  </NavigationContainer>
+);
